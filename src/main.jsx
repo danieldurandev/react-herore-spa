d380c7d5f2dc5805afd71ad7f414f039ff4b8e52
Daniel Durán <danieldurange@outlook.com>
2023-04-12 21:49:54 -0500

    01-commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HeroesApp } from "./HeroesApp";
+import "./bootstrap.min.css";
+import "./styles.css";
+import "animate.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <HeroesApp />
+    </React.StrictMode>
+  </BrowserRouter>
+);
