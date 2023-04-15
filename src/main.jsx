2118fa45357056c4b4ed745dc03e5c60df9aa763
Daniel Durán <danieldurange@outlook.com>
2023-04-15 17:38:12 -0500

    03-BrowserRouter implementado

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { HeroesApp } from "./HeroesApp";
 import "./bootstrap.min.css";
 import "./styles.css";
 import "animate.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <BrowserRouter>
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <HeroesApp />
     </React.StrictMode>
-  </HashRouter>
-  // </BrowserRouter>
+  </BrowserRouter>
 );
